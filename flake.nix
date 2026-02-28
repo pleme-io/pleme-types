f25606a56454328c6f25b0f7398d5badda44ae2d
drzzln <drzzln@protonmail.com>
2026-02-27 21:12:09 -0500

    chore: switch nixpkgs to nixos-25.11 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Shared TypeScript types for Nexus products";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     dream2nix = {
       url = "github:nix-community/dream2nix";
       inputs.nixpkgs.follows = "nixpkgs";
